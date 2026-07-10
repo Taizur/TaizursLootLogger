c943dd7a2b1e26a309c3627df6a7f00c974fe79b
Brandon Turner <brandonturner4677@gmail.com>
2026-07-10 05:41:52 -0500

    Rename plugin and configure project paths

--- src/test/java/com/taizur/lootlogger/TaizursLootLoggerPluginTest.java
+++ src/test/java/com/taizur/lootlogger/TaizursLootLoggerPluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.taizur.lootlogger;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class TaizursLootLoggerPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(TaizursLootLoggerPlugin.class);
 		RuneLite.main(args);
 	}
 }
